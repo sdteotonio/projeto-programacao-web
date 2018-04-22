d330664dd42f27e5045d06c25a6f78d5a6aba883
Sergio Davi Marinho <sergioivad@hotmail.com>
2018-04-22 16:48:30 -0300

    Classe DAO

---
+++ ufab-biblioteca/src/main/java/com/ufab/dao/DAO.java
@@ -0,0 +1,14 @@
+package com.ufab.dao;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public abstract class DAO {
+	@Autowired
+	protected SessionFactory sessionFactory;
+
+	protected Session getCurrentSession() {
+		return sessionFactory.getCurrentSession();
+	}
+}
