59dd43768698afeb168db0691745d472167570d8
Sergio Davi Marinho <sergioivad@hotmail.com>
2018-05-20 23:39:55 -0300

    Ajustes no DML pos e Login

--- Modelagem/DataBase/M_SQL/dml_pos_ufab.sql
+++ Modelagem/DataBase/M_SQL/dml_pos_ufab.sql
@@ -25,3 +25,5 @@ LOCK TABLES `usuario` WRITE;
 INSERT INTO `usuario` VALUES ('11772699454','2018-01-20 00:00:00','1996-10-10 00:00:00','Endereco Teste Adm','83 9 93162731','Brasileiro','Sergio Davi Marinho','38698399','teste',1);
 
 UNLOCK TABLES;
+
+INSERT INTO `administrador` (`usuario_cpf`) VALUES ('11772699454');
